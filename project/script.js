8fd325a22bc53e75777933396656dc3eda0f04f8
Lucas Gabriel Xavier <lks.gabriel2013@gmail.com>
2024-03-01 12:01:57 -0300

    :tada: inicial commit

---
+++ project/script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    const html = document.documentElement
+  html.classList.toggle('light')
+  
+   //pega a tag img 
+   const img = document.querySelector('#profile img')
+
+   // substituir a img
+    if (html.classList.contains('light')) {
+
+         img.setAttribute("src", './assets/imgavatar-light.png')
+    //se tiver light mode add a img light
+    } else {
+        img.setAttribute("src", "./assets/avatar.png")
+    //se nao manter img normal 
+    }
+}
